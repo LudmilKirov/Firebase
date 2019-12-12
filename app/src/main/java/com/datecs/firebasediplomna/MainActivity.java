0bb351933e49b8fd9e228244a986218cb44a97bf
LudmilKirov <ludmil9595@gmail.com>
2019-12-12 18:24:33 +0200

    refactor

--- app/src/main/java/com/datecs/firebasediplomna/MainActivity.java
+++ app/src/main/java/com/datecs/firebasediplomna/MainActivity.java
@@ -14,9 +14,13 @@ import android.graphics.ColorSpace;
 import android.hardware.camera2.params.BlackLevelPattern;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -49,58 +53,48 @@ import static android.graphics.ColorSpace.Model.RGB;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Tag";
-    private static final int MAX_TEMP=21;
+    private static final int MAX_OUTPUT_VOLTAGE = 6;
+    private static final int JOB_ID = 0;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef ;
-    public static final String EXTRA_DATE = "com.example.criminalintent";
-    private static final String ARG_DATE = "date";
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef;
     private DatePicker mDatePicker;
     private Button mOkButton;
     private String requiredDate;
-    DateInfo dateInfo = new DateInfo();
-    PieChart pieChart;
-    List<PieEntry> value = new ArrayList<>();
-    PieData pieData;
-    Date date2;
-    Description description=new Description();
-    float averageTemperature;
-    float averageInput;
-    float averageOutput;
-    int connectionCount;
-    boolean isConnect=false;
+    private DateInfo dateInfo = new DateInfo();
+    private PieChart pieChart;
+    private List<PieEntry> value = new ArrayList<>();
+    private PieData pieData;
+    private Date date2;
+    private Description description = new Description();
+    private float averageTemperature;
+    private float averageInput;
+    private float averageOutput;
+    private int connectionCount;
+    private boolean wantNotification;
+    private boolean isConnect = false;
     private SimpleDateFormat formater = new SimpleDateFormat("dd MM yyyy kk:mm:ss:SSSS");
-
-    private static final int JOB_ID = 0;
-
-
     private JobScheduler scheduler;
-
-
-//    final DatabaseReference temperature = myRef.child("Temp").child("value");
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        myRef = database.getReference();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (wantNotification) {
+                    scheduleJob(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
         pieChart = findViewById(R.id.piechart);
-       //pieChart.setUsePercentValues(true);
-
-
-
-//        value.add(new PieEntry(40f,"jan"));
-//        value.add(new PieEntry(30f,"feb"));
-//        Log.d(TAG, "onCreate: kur"+value);
-//
-
-
-
-
         // create a Calendar object and use the Date to configure
         // the Calendar.Then initialize the DatePicker.
         Calendar calendar = Calendar.getInstance();
@@ -120,68 +114,45 @@ public class MainActivity extends AppCompatActivity {
                 int month1 = mDatePicker.getMonth();
                 int day1 = mDatePicker.getDayOfMonth();
                 Date date1 = new GregorianCalendar(year1, month1, day1).getTime();
-                long requoredNumber = date1.getTime();
-                date2=new Date(requoredNumber);
-                requiredDate=Long.toString(requoredNumber);
-                requiredDate.substring(0,5);
-                myRef = database.getReference();
-                Log.d(TAG, "onClick: kur"+requiredDate);
-                if (requiredDate != null) {
+                long requiredTime = date1.getTime();
+                date2 = new Date(requiredTime);
+                requiredDate = Long.toString(requiredTime);
 
-                    myRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            //if(requiredDate!=null){
-                            Log.d(TAG, "onDataChange: kurbaza" + requiredDate);
-                            showData(dataSnapshot);
-                            //}
-                        }
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        showData(dataSnapshot);
+                    }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-                }
-
-
-              //  Log.d(TAG, "onClick:kurjelanipurvi 4 cifri "+requiredDate.substring(0,5));
-//                Log.d(TAG, "onClick: kur "+formater.format(date1.getTime()));
-//                Log.d(TAG, "onClick: 1kur 1575395817631 " +formater.format(date1.getTime()));
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
             }
         });
-
-
-//        myRef.startAt(15753).
     }
 
     private void showData(DataSnapshot dataSnapshot) {
-        Log.d(TAG, "showData: kur");
+        String selectedDate = date2.toString().substring(4, 10).concat(date2.toString().substring(30, 34));
 
-
-        String selectedDate = date2.toString().substring(4,10).concat(date2.toString().substring(30,34));
-            for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                   if(!(ds.getKey().equals("DataForMonitoring"))){
-                Log.d(TAG, "showData: kurkey" + ds.getKey());
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            if (!(ds.getKey().equals("DataForMonitoring"))) {
 
                 Long dateLong = Long.valueOf(ds.getKey());
-                Date date=new Date(dateLong);
+                Date date = new Date(dateLong);
                 //String currentDate = String.valueOf(date.getDay()+date.getMonth()+date.getYear());
-                String currentDate = date.toString().substring(4,10).concat(date.toString().substring(30,34));
-                Log.d(TAG, "showData: date1"+currentDate);
+                String currentDate = date.toString().substring(4, 10).concat(date.toString().substring(30, 34));
 
-                Log.d(TAG, "date1 i date"+" " +date2+date2.getDay() +" " + date2.getMonth()+" "+date2.getYear()+" "+dateLong+ "        "+ date+" "+date.getMonth()+" "+date.getYear());
                 if (selectedDate.equals(currentDate)) {
-                 isConnect=true;
+                    isConnect = true;
                     connectionCount++;
-                    Log.d(TAG, "showData: kurvata   " + ds.getValue());
 
                     if (ds.getValue().toString().contains(".")) {
                         dateInfo.setTemperature(ds.getValue().toString().substring(12, 13));
                     } else {
-                        averageTemperature+=Integer.valueOf(ds.getValue().toString().substring(13, 15));
-                        averageInput+=Integer.valueOf(ds.getValue().toString().substring(31, 33));
-                        averageOutput+=Integer.valueOf(ds.getValue().toString().substring(50, 51));
+                        averageTemperature += Integer.valueOf(ds.getValue().toString().substring(13, 15));
+                        averageInput += Integer.valueOf(ds.getValue().toString().substring(31, 33));
+                        averageOutput += Integer.valueOf(ds.getValue().toString().substring(50, 51));
 
                         dateInfo.setTemperature(ds.getValue().toString().substring(13, 15));
                         dateInfo.setOutputVoltage(ds.getValue().toString().substring(31, 33));
@@ -189,90 +160,100 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-            }
-            averageOutput/=connectionCount;
-            averageInput/=connectionCount;
-            averageTemperature/=connectionCount;
+        }
+        averageOutput /= connectionCount;
+        averageInput /= connectionCount;
+        averageTemperature /= connectionCount;
 
-            if(isConnect) {
-              //  BarData barData = new BarData((List<IBarDataSet>) pieDataSet);
-               // barData.setBarWidth(0.33f);
+        if (isConnect) {
+            //  BarData barData = new BarData((List<IBarDataSet>) pieDataSet);
+            // barData.setBarWidth(0.33f);
 
-                description.setText("Average data");
-                description.setTextSize(20f);
-                pieChart.setDescription(description);
-                pieChart.setHoleRadius(0f);
-                pieChart.setTransparentCircleRadius(0f);
+            description.setText("Average data");
+            description.setTextSize(20f);
+            pieChart.setDescription(description);
+            pieChart.setHoleRadius(0f);
+            pieChart.setTransparentCircleRadius(0f);
 
+            value.clear();
+            value.add(new PieEntry(averageOutput, "Ouput"));
+            value.add(new PieEntry(averageTemperature, "Temperature"));
+            value.add(new PieEntry(averageInput, "Input"));
 
+            PieDataSet pieDataSet = new PieDataSet(value, "");
+            // add a lot of colors
 
+            ArrayList<Integer> colors = new ArrayList<>();
 
+            for (int c : ColorTemplate.PASTEL_COLORS)
+                colors.add(c);
 
-                value.clear();
-                value.add(new PieEntry(averageOutput, "Ouput"));
-                value.add(new PieEntry(averageTemperature, "Temperature"));
-                value.add(new PieEntry(averageInput, "Input"));
+            colors.add(ColorTemplate.getHoloBlue());
 
-               PieDataSet  pieDataSet = new PieDataSet(value, "");
-                // add a lot of colors
+            pieDataSet.setColors(colors);
+            PieData data = new PieData(pieDataSet);
+            data.setValueFormatter(new PercentFormatter(pieChart));
+            data.setValueTextSize(11f);
+            data.setValueTextColor(Color.WHITE);
+            pieChart.setData(data);
+            pieData = new PieData(pieDataSet);
 
-                ArrayList<Integer> colors = new ArrayList<>();
-
-
-                for (int c : ColorTemplate.PASTEL_COLORS)
-                    colors.add(c);
-
-                colors.add(ColorTemplate.getHoloBlue());
-
-                pieDataSet.setColors(colors);
-                PieData data = new PieData(pieDataSet);
-                data.setValueFormatter(new PercentFormatter(pieChart));
-                data.setValueTextSize(11f);
-                data.setValueTextColor(Color.WHITE);
-             //   data.setValueTypeface(tfLight);
-                pieChart.setData(data);
-                pieData = new PieData(pieDataSet);
-
-               // pieChart.setData(pieData);
-
-
-               pieDataSet.setColors(ColorTemplate.PASTEL_COLORS);
-                pieChart.setEntryLabelColor(0);
-                pieChart.animateXY(1400, 1400);
-                pieDataSet.setDrawIcons(false);
-
-
-
-                // entry label styling
-             //   pieChart.setEntryLabelColor(Color.BLACK);
-                //   pieChart.setEntryLabelTypeface(tfRegular);
-               // pieChart.setEntryLabelTextSize(12f);
-                Log.d(TAG, "showData:kurvalue   " + value);
-                Log.d(TAG, "showData: kurec " + dateInfo.getTemperature());
-                scheduleJob();
-
-            }
-        averageTemperature=0;
-        averageInput=0;
-        averageOutput=0;
-        connectionCount=0;
+            pieDataSet.setColors(ColorTemplate.PASTEL_COLORS);
+            pieChart.setEntryLabelColor(0);
+            pieChart.animateXY(1400, 1400);
+            pieDataSet.setDrawIcons(false);
+        }
+        averageTemperature = 0;
+        averageInput = 0;
+        averageOutput = 0;
+        connectionCount = 0;
     }
 
-
-    private void scheduleJob(){
+    private void scheduleJob(DataSnapshot dataSnapshot) {
         scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        ComponentName serviceName = new ComponentName(getPackageName(),NotificationJobService.class.getName());
-        JobInfo.Builder builder=new JobInfo.Builder(JOB_ID,serviceName)
+        ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-        int temp = 23;
-        boolean constraintSet = MAX_TEMP<temp;
+        int temp = 21;
+
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            if ((ds.getKey().equals("DataForMonitoring"))) {
+                int endValue = ds.getValue().toString().length() - 1;
+                temp = Integer.parseInt(ds.getValue().toString().substring(8, endValue));
+            }
+        }
+        boolean constraintSet = MAX_OUTPUT_VOLTAGE < temp;
+
         if (constraintSet) {
             JobInfo myJobInfo = builder.build();
             scheduler.schedule(myJobInfo);
-            Toast.makeText(this,R.string.google_app_id,Toast.LENGTH_SHORT).show();
         }
     }
 
-  //  String condition = "Otput ''"
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem item = (MenuItem) menu.findItem(R.id.switchId);
+        item.setActionView(R.layout.switch_layout);
+        Switch switchAB = item
+                .getActionView().findViewById(R.id.switchAB);
+        switchAB.setChecked(true);
 
+        switchAB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                wantNotification = isChecked;
+                if (isChecked) {
+                    Toast.makeText(getApplication(), "ON", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    Toast.makeText(getApplication(), "OFF", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+        return true;
+    }
 }
--- app/src/main/java/com/datecs/firebasediplomna/NotificationJobService.java
+++ app/src/main/java/com/datecs/firebasediplomna/NotificationJobService.java
@@ -1,10 +1,14 @@
 package com.datecs.firebasediplomna;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -22,30 +26,46 @@ public class NotificationJobService extends JobService {
      */
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-
         //Set up the notification content intent to launch the app when clicked
         PendingIntent contentPendingIntent = PendingIntent.getActivity
                 (this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        //For Androids after Oreo is needed a second channel
+        if (Build.VERSION.SDK_INT >= 26) {
+            String id = "channel_1";
+            String description = "143";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(id, description, importance);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            manager.createNotificationChannel(channel);
+            Notification notification = new Notification.Builder(this, id)
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    .setSmallIcon(R.drawable.ic_warning_black_24dp)
+                    .setContentTitle("Warning")
+                    .setContentText("Bigger value than expected is measured")
+                    .setContentIntent(contentPendingIntent)
+                    .setAutoCancel(true)
+                    .build();
+            manager.notify(1, notification);
+        } else {
+            //When sdk version is less than 26
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentTitle("Warning")
+                    .setContentText("Bigger value than expected is measured")
+                    .setContentIntent(contentPendingIntent)
+                    .setSmallIcon(R.drawable.ic_warning_black_24dp)
+                    .build();
+            manager.notify(1, notification);
+        }
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setContentTitle("Bepche")
-                .setContentText("Zdr")
-                .setContentIntent(contentPendingIntent)
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setAutoCancel(true);
-
-        manager.notify(0, builder.build());
-
-        return false;
+        return true;
     }
 
     /**
      * Called by the system when the job is running but the conditions are no longer met.
      * In this example it is never called since the job is not offloaded to a different thread.
+     *
      * @param jobParameters Contains the information about the job
      * @return Boolean indicating whether the job needs rescheduling
      */
